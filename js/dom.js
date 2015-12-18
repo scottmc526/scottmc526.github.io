070d3cdcdb82cd73488e350fe91e5d2c6c32cbb9
Scott Christensen <scottmc526@gmail.com>
2015-12-17 18:52:09 -0700

    finished

--- js/dom.js
+++ js/dom.js
@@ -16,4 +16,21 @@ $(document).ready(function (){
         $('.name').empty().append('<p class="person">'+firstNamey+' '+lastNamey+'</p>')
       })
     })
+
+    $('.click2').click(function (){
+      var getter = $.ajax ({
+        url: "https://randomuser.me/api/",
+        method: 'GET',
+        dataType: 'json'
+      })
+      getter.done(function (returned){
+        var result = returned['results'][0]['user']['picture']['thumbnail'];
+        var firstNamey = returned['results'][0]['user']['name']['first'];
+        var lastNamey = returned['results'][0]['user']['name']['last'];
+
+
+        $('.pic2').empty().append('<img src="'+result+'"/>')
+        $('.name2').empty().append('<p class="person">'+firstNamey+' '+lastNamey+'</p>')
+      })
+    })
 })
